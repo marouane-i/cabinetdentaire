e7c51dbe63d5bf8fd53ede23068ef4072986435d
unknown <youssefbahomman3@gmail.com>
2022-05-20 22:56:05 +0100

    structure files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatMenuModule} from '@angular/material/menu';
-import { MainFooterComponent } from './main-footer/main-footer.component';
-import { AccueilComponent } from './accueil/accueil.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 import {MatCardModule} from '@angular/material/card';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MainComponent } from './pages/main/main.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
     MainFooterComponent,
-    AccueilComponent
+    AccueilComponent,
+    MainComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
